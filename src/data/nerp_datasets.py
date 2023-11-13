Fixed ssim for new normalization, added normalization which helps wire to train better, modified configs

src/data/nerp_datasets.py
@@ -30,28 +30,28 @@ def complex_center_crop(data, shape):
     h_to = h_from + shape[1]
     return data[..., w_from:w_to, h_from:h_to, :]
 
-def normalize_image_2(data, full_norm=False):
+def normalize_image(data, full_norm=False):
     
     C,_,_,_ = data.shape
     data_flat = data.reshape(C,-1)
-    norm = torch.abs(data_flat).max(dim=-1)[0].unsqueeze(dim=-1).unsqueeze(dim=-1).unsqueeze(dim=-1)
+    norm = torch.abs(data_flat).max()
     return data/norm 
 
-def normalize_image_1(data, full_norm=False):
+# def normalize_image_1(data, full_norm=False):
     
-    mean = data.mean()
-    std = data.std()
-    return (data - mean) / (std)
+#     mean = data.mean()
+#     std = data.std()
+#     return (data - mean) / (std)
 
-def normalize_image(data, full_norm=True):
-    re = data[:,:,:,0]
-    im = data[:,:,:,1]
-    re_min = re.min()
-    im_min = im.min()
-    re = 2 * (re - re_min)/(re.max() - re_min) -1
-    im = 2 * (im - im_min)/(im.max() - im_min) -1
-    data = torch.stack((re,im),dim=-1)
-    return data
+# def normalize_image(data, full_norm=True):
+#     re = data[:,:,:,0]
+#     im = data[:,:,:,1]
+#     re_min = re.min()
+#     im_min = im.min()
+#     re = 2 * (re - re_min)/(re.max() - re_min) -1
+#     im = 2 * (im - im_min)/(im.max() - im_min) -1
+#     data = torch.stack((re,im),dim=-1)
+#     return data
 
     
 def create_grid_3d(c, h, w):

src/models/networks.py
@@ -188,9 +188,9 @@ class ComplexGaborLayer(nn.Module):
 class WIRE(nn.Module):
     def __init__(self, 
                  params,
-                 first_omega_0=30, 
-                 hidden_omega_0=30, 
-                 scale=30.0,
+                 first_omega_0=10, 
+                 hidden_omega_0=10, 
+                 scale=10.0,
                 ):
         super().__init__()
         

src/models/utils.py
@@ -11,8 +11,8 @@ def get_device(net_name):
     device = ("cuda" if torch.cuda.is_available() else 
                         ("mps" if torch.backends.mps.is_available() else "cpu"))
 
-    if "WIRE" in net_name and device == "mps":
-        return torch.device("cpu")
+    # if "WIRE" in net_name and device == "mps":
+    #     return torch.device("cpu")
     return torch.device(device)
 
 def get_config(config):
@@ -98,7 +98,7 @@ def ssim(x, xhat):
         x = x.numpy()
     if torch.is_tensor(xhat):
         xhat = xhat.numpy()
-    return structural_similarity(x,xhat)
+    return structural_similarity(x,xhat, data_range=xhat.max()-xhat.min())
 
 def psnr(x, xhat, epsilon=1e-10):
     ''' Compute Peak Signal to Noise Ratio in dB