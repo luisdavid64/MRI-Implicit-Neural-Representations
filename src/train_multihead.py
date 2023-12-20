5a4591287623f17f9aa18c6db01065b55bff2132
luisdavid64 <luisdavid007@gmail.com>
2023-12-19 19:34:26 -0600

    Model simplification

--- src/train_multihead.py
+++ src/train_multihead.py
@@ -180,7 +180,7 @@ for epoch in range(max_epoch):
             ind = torch.where((dist_to_center >= r_0) & (dist_to_center <= r_1))
             if ind[0].numel():
                 coords_local, gt_local, dists_local = coords[ind], gt[ind], dist_to_center[ind]
-                layer_outs, train_output = model(coords_local, i)
+                layer_outs, train_output = model(coords_local)
                 train_loss = 0
                 for idx, out in enumerate(layer_outs):
                     # Get gradients for final layers, and scale if target
@@ -223,7 +223,7 @@ for epoch in range(max_epoch):
                     ind = torch.where((dist_to_center >= r_0) & (dist_to_center <= r_1))
                     if ind[0].numel():
                         coords_local, gt_local, dists_local = coords[ind], gt[ind], dist_to_center[ind]
-                        _,test_output = model(coords_local, i)
+                        _,test_output = model(coords_local)
                         test_loss = 0
                         if config["loss"] in ["HDR", "LSL", "FFL", "tanh"]:
                             test_loss, _ = loss_fn(test_output, gt_local, coords.to(device))
