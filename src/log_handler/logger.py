f3395c6121dc6ff5240c10eaf1729bd6eb18431f
luisdavid64 <luisdavid007@gmail.com>
2024-01-25 14:53:28 +0100

    Made class for logging results on tensorboard

---
+++ src/log_handler/logger.py
@@ -0,0 +1,13 @@
+import torch.utils.tensorboard as tensorboardX
+
+class INRLogger():
+    def __init__(self, log_dir):
+        self.writer = tensorboardX.SummaryWriter(log_dir=log_dir)
+
+    def log_train(self, loss, epoch):
+        self.writer.add_scalar("train_loss", loss, epoch)
+    
+    def log_test(self, loss, psnr, ssim, epoch):
+        self.writer.add_scalar('test_loss', loss, epoch)
+        self.writer.add_scalar('test_psnr', psnr, epoch)
+        self.writer.add_scalar('test_ssim', ssim, epoch)
