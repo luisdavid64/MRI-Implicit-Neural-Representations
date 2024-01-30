ecc96d1d22eca76c90b7e801194a279fbefb42ca
luisdavid64 <luisdavid007@gmail.com>
2024-01-30 16:01:25 +0100

    Potential error fix

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,8 @@ def set_default_configs(config):
     if "use_tv" not in config:
         config["use_tv"] = False
     if "regularization" not in config:
-        config["regularization"] = {"type": "none"}
+        config["regularization"] = dict()
+        config["regularization"]["type"] = "none" 
     if "undersampling" not in config:
         config["undersampling"] = "none"
 
